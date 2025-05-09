f240390b9efa5c9dea484b04a5ac3ccd2f4593ea
anuradha1705 <anuradhakumawat2005@gmail.com>
2025-05-09 21:51:59 +0530

    first commit

--- AddTwoNumbers.java
+++
@@ -1,7 +0,0 @@
-public class AddTwoNumbers {
-    public static void main(String[] args) {
-        int a=2,b=4,sum=a+b;
-        System.out.println("sum:"+sum);
-    }
-}
-    
---
+++ multiplication.java
@@ -0,0 +1,6 @@
+public class multiplication {
+    public static void main(String[] args) {
+        int a=2,b=2,mul=a*b;
+        System.out.println("multiplication of two numbers:"+ mul);
+    }
+}
