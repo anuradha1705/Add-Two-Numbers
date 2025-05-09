f79b55b4bec55028f5de888b778152a0271bd816
anuradha1705 <anuradhakumawat2005@gmail.com>
2025-05-09 21:21:29 +0530

    add only multiplication file

--- Add.java
+++
@@ -1,7 +0,0 @@
-public class Add {
-
-    public static void main(String[] args) {
-        int a=10, b=5,sum=a+b;
-        System.out.println("sum:"+sum);
-    }
-}
---
+++ New folder/multipliacation.java
@@ -0,0 +1,7 @@
+public class multipliacation {
+
+    public static void main(String[] args) {
+        int a=2,b=3,mul=a*b;
+        System.out.println("multiplication:"+mul);
+    }
+}
