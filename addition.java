51b9da2f06b316c65dedcfe180a3b1850a1c6dcc
anuradha1705 <anuradhakumawat2005@gmail.com>
2025-05-10 06:52:08 +0530

    addition program

---
+++ addition.java
@@ -0,0 +1,6 @@
+public class addition {
+ public static void main(String[] args) {
+    int a=2,b=2,sum=a+b;
+    System.out.println("sum:"+sum);
+ }   
+}
--- multiplication.java
+++
@@ -1,6 +0,0 @@
-public class multiplication {
-    public static void main(String[] args) {
-        int a=2,b=2,mul=a*b;
-        System.out.println("multiplication of two numbers:"+ mul);
-    }
-}
